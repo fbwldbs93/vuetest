68cddee81c2324ca3f91b709ddf68dcf60d79bbd
june.ryu <june.ryu@3i.ai>
2021-09-07 17:12:24 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--<img alt="Vue logo" src="./assets/logo.png">-->
+  <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <test/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import test from './components/test.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    test
   }
 }
 </script>
---
+++ src/components/test.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="app">
+        <h1>{{ test }}</h1>
+        <h1>{{ test2 }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    /*
+    name: 'Test',
+    props: {
+        msg: {
+            type:String
+        }
+    }
+    */
+   data(){
+       return {
+           test : "hello", 
+           test2 : "bye"
+       }
+   },
+   
+}
+</script>
